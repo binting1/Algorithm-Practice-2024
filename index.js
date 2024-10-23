6267b5eef4ff0b1d3977d76d80e57dcb0a363694
binting1 <bintingchenn123@gmail.com>
2024-10-23 13:58:40 -0400

    123

--- index.js
+++ index.js
@@ -9,5 +9,23 @@
 //      return result;
 // }
 
-const N = 6; 
+// function checkSpaces(n, yesterday, today) {
+//     let count = 0; 
+//     for(let i= 0; i < n; i++) {
+//         if(yesterday[i] === 'C' && today[i] === 'C') {
+//             count++; 
+//         }
+//     }
+//     return count;
+// }
 
+// console.log(checkSpaces(5, "CCC.C", ".CC.." ));
+
+function DtoB (n, decimal, binary) {
+    for (let i=0, i < n, i++) {
+        if(decimal[i] === )
+    }
+    return binary 
+}
+
+2.45
